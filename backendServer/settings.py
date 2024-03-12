82f833013098a369ce535f25659a7d41b0f6fd99
Ron0324 <naldosan1013@gmail.com>
2024-03-12 22:29:43 +0800

    update

--- backendServer/settings.py
+++ backendServer/settings.py
@@ -106,8 +106,8 @@ CORS_ALLOW_METHODS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'http://127.0.0.1:8000',
-    'http://52.63.82.248/',
-    'http://13.239.99.118:8000/',
+    'http://52.63.82.248',
+    'http://13.239.99.118:8000',
     
 ]
 
