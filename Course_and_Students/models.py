2185d7e00cb032a9bf1963f272ed7bb2b8067090
Ron0324 <naldosan1013@gmail.com>
2024-04-30 07:52:59 +0800

    update

--- Course_and_Students/models.py
+++ Course_and_Students/models.py
@@ -31,7 +31,7 @@ class Student(AbstractUser):
 class Admin(AbstractUser):
     full_name = models.CharField(max_length=150, unique=False) 
     id_number = models.CharField(max_length=20, unique=True)
-
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
     def __str__(self):
         return f"{self.full_name} ({self.id_number})"
 
